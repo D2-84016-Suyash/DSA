1dcae020c386024be798a4c02a5fe83d1678c05e
D2_suyash_84016 <patilsuyash1177@gmail.com>
2024-06-23 13:43:42 +0530

    Assignment5 Q1 completed

---
+++ Assignment-5/Question_1/Progeam.java
@@ -0,0 +1,13 @@
+package Question_1;
+
+public class Progeam {
+public static void main(String[] args) {
+	BST bin = new BST();
+	bin.addNode(5);
+	bin.addNode(10);
+	bin.addNode(4);
+	bin.addNode(16);
+	bin.addNode(9);
+	bin.preorder();
+}
+}
